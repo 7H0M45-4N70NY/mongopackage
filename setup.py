add2224c40d64ff24525a651d5883356ed23399e
7H0M45-4N70NY <thomasantony14@gmail.com>
2024-01-04 21:13:55 +0530

    removed get requirements from setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,5 @@
 from setuptools import setup, find_packages
 from typing import List
-# from test import get_requiremet
-
-HYPEN_E_DOT='-e .'
-
-def get_requiremet(file_path:str)->List[str]:
-    requirements = []
-    with open(file_path) as f:
-        requirements=f.readlines()
-        requirements=[req.replace("\n","")for req in requirements]
-        
-        if HYPEN_E_DOT in requirements:
-            requirements.remove(HYPEN_E_DOT)
-    return requirements
-
-print(get_requiremet("./requirements.txt"))
-
-
 
 with open('README.MD', 'r', encoding='utf-8') as f:
     long_description = f.read()     
