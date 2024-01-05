0583d6e9d2bfedf1d9693a55b37c05597678f862
7H0M45-4N70NY <thomasantony14@gmail.com>
2024-01-05 16:09:39 +0530

    rock on

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 from typing import List
 
-with open('README.md', 'r', encoding='utf-8') as f:
-    long_description = f.read()     
+#with open('README.md', 'r', encoding='utf-8') as f:
+    #long_description = f.read()     
    
 
 __version__ = "0.0.5"
@@ -17,7 +17,7 @@ setup(
     author=AUTHOR_USER_NAME,
     author_email=AUTHOR_EMAIL,
     description="A python package for connecting with database.",
-    long_description=long_description,
+    long_description="no description",
     long_description_content="text/markdown",
     url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
     project_urls={
